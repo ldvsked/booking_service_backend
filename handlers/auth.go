0013081123b6558219b7593fbe6f0e71fb3e7e04
authentk <authentk@msk-school.21>
2026-04-03 17:18:54 +0300

    feat: rooms/create

--- handlers/auth.go
+++ handlers/auth.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"time"
+	"github.com/internships-backend/test-backend-ldvsked/models"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
@@ -81,8 +82,38 @@ func AuthMiddleware(c *gin.Context) {
 		return
 	}
 	
-
-
-	//проверить role 
-	//если все ок положить в контекст и передатб управление дальше
+	c.Set("userId", userId)
+	c.Set("role", role)
+	c.Next()
 }
+
+type CreateRoomRequest struct {
+	Name string `json:"name"`
+	Capacity int `json:"capacity"`
+	Description string `json:"description"`
+}
+
+func CreateRoom(c *gin.Context) {
+	role := c.GetString("role")
+
+	if role != "admin" {
+		c.JSON(403, map[string]any{"error":"forbidden"})
+		return
+	}
+
+	var createRoomRequest CreateRoomRequest
+	//ошибка в синтаксисе, невалидные типы
+	if err := c.ShouldBindJSON(&createRoomRequest); err != nil || createRoomRequest.Name == ""{
+		c.JSON(400, map[string]any{"error" : "invalid request"})
+		return
+	}
+	newRoom := models.Room{Id: uuid.New(), 
+		Name: createRoomRequest.Name,
+		Description: createRoomRequest.Description,
+		Capacity: createRoomRequest.Capacity,
+		CreatedAt: time.Now(),
+	}
+	models.Rooms = append(models.Rooms, newRoom)
+	c.JSON(201, map[string]any{"room": newRoom})
+}
+
--- main.go
+++ main.go
@@ -6,20 +6,16 @@ import (
 	"github.com/internships-backend/test-backend-ldvsked/handlers"
 )
 
-func SayHi(c *gin.Context) {
-	c.JSON(200, map[string]interface{}{"message": "Hi, girls!"})
-}
-
 
 func main() {
 	var r *gin.Engine = gin.Default()
 
 	r.Use(cors.Default())
 
-	r.GET("/say_hi", SayHi)
 	r.GET("/rooms/list", handlers.GetRooms)
 	r.POST("/dummyLogin", handlers.DummyLogin)
 	r.GET("/_info", handlers.Info)
+	r.POST("/rooms/create", handlers.AuthMiddleware, handlers.CreateRoom)
 
 	r.Run("localhost:8080")
 
