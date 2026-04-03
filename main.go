5097df10295f889f34efead669e8044665803374
authentk <authentk@msk-school.21>
2026-04-03 19:22:14 +0300

    fix: cors

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/gin-contrib/cors" // приклеивать разрешающие заголовки, чтобы браузер не ругался, сервер разрешает использовать его данные
 	"github.com/gin-gonic/gin"
-	"github.com/internships-backend/test-backend-ldvsked/rooms"
+	"github.com/internships-backend/test-backend-ldvsked/handlers/rooms"
 	"github.com/internships-backend/test-backend-ldvsked/handlers"
 	
 )
@@ -12,12 +12,17 @@ import (
 func main() {
 	var r *gin.Engine = gin.Default()
 
-	r.Use(cors.Default())
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"}, 
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"}, 
+		AllowCredentials: true,
+	}))
 
-	r.GET("/rooms/list", handlers.GetRooms)
+	r.GET("/rooms/list", rooms.GetRooms)
 	r.POST("/dummyLogin", handlers.DummyLogin)
 	r.GET("/_info", handlers.Info)
-	r.POST("/rooms/create", handlers.AuthMiddleware, handlers.CreateRoom)
+	r.POST("/rooms/create", handlers.AuthMiddleware, rooms.CreateRoom)
 
 	r.Run("localhost:8080")
 
